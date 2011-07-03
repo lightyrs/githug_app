cd2df3dbf62e4341dbdaa7c2b2b4a72c2d5accf0
lightyrs <harris@harrisnovick.com>
2011-07-03 15:42:16 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githug::Application.config.secret_token = 'e44b566f4f819e82979c180b1bf94018af7830ed0b5ae37c00c3c7cccd3913e69d1e046d89e5d145268882e0ae20ada509863d15f5eb938318788619de87322a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Githug::Application.config.session_store :cookie_store, :key => '_githug_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Githug::Application.config.session_store :active_record_store
